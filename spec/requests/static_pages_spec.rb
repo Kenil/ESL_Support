32b772bbf34b8a7a8dccc0e7d5fb5c2c6311da79
Kenil <kenil.p.bhatt@gmail.com>
2014-02-09 17:50:46 -0700

    Add Contact Us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  let(:base_title) { "ESL Support" }
+  
   # Test case belongs to a home page
   describe "Home page" do
     
@@ -17,7 +19,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("ESL Support | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -30,7 +32,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("ESL Support | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -43,7 +45,20 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("ESL Support | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  
+  describe "Contact Us page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
     end
   end
 end
